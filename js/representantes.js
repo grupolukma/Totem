9c09902565e815c69134120d6ae6b72e8f2decc2
tilukma <143527639+grupolukma@users.noreply.github.com>
2024-01-19 17:21:57 -0300

    teste

--- js/representantes.js
+++ js/representantes.js
@@ -1,11 +1,27 @@
-document.addEventListener("DOMContentLoaded", function () {
-    var elementoSP = document.getElementById("state-sp");
-    var paragrafoMensagem = document.getElementById("mensagem");
+document.getElementById('state-sp').addEventListener('click', function() {
+    document.getElementById('mensagem').innerText = 'Bem-vindo a São Paulo!';
+});
 
-    if (elementoSP && paragrafoMensagem) {
-        elementoSP.addEventListener("click", function () {
-            // Atualiza o conteúdo do parágrafo com a mensagem de boas-vindas
-            paragrafoMensagem.textContent = "Bem-vindo a São Paulo!";
-        });
-    }
+document.getElementById('state-ce').addEventListener('click', function() {
+    document.getElementById('mensagem').innerText = 'Bem-vindo ao Ceará!';
+});
+
+document.getElementById('state-rr').addEventListener('click', function() {
+    document.getElementById('mensagem').innerText = 'Bem-vindo a Roraima!';
+});
+
+document.getElementById('state-to').addEventListener('click', function() {
+    document.getElementById('mensagem').innerText = 'Bem-vindo ao Tocantins!';
+});
+
+document.getElementById('state-ma').addEventListener('click', function() {
+    document.getElementById('mensagem').innerText = 'Bem-vindo ao Maranhão!';
+});
+
+document.getElementById('state-rn').addEventListener('click', function() {
+    document.getElementById('mensagem').innerText = 'Bem-vindo ao Rio Grande do Norte!';
+});
+
+document.getElementById('state-pb').addEventListener('click', function() {
+    document.getElementById('mensagem').innerText = 'Bem-vindo à Paraíba!';
 });
