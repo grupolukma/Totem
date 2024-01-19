3797d3dfc6fd1b810ff7c644cd2daee5e7f5ff70
tilukma <143527639+grupolukma@users.noreply.github.com>
2024-01-19 17:36:33 -0300

    estados

--- js/representantes.js
+++ js/representantes.js
@@ -1,27 +1,107 @@
+document.getElementById('state-df').addEventListener('click', function() {
+    document.getElementById('mensagem').innerText = 'Distrito Industrial | Brasília';
+});
+
+document.getElementById('state-rs').addEventListener('click', function() {
+    document.getElementById('mensagem').innerText = 'Rio Grande do Sul';
+});
+
+document.getElementById('state-sc').addEventListener('click', function() {
+    document.getElementById('mensagem').innerText = 'Santa Catarina';
+});
+
+document.getElementById('state-pr').addEventListener('click', function() {
+    document.getElementById('mensagem').innerText = 'Paraná';
+});
+
 document.getElementById('state-sp').addEventListener('click', function() {
-    document.getElementById('mensagem').innerText = 'Bem-vindo a São Paulo!';
+    document.getElementById('mensagem').innerText = 'São Paulo';
+});
+
+document.getElementById('state-mg').addEventListener('click', function() {
+    document.getElementById('mensagem').innerText = 'Minas Gerais';
+});
+
+document.getElementById('state-rj').addEventListener('click', function() {
+    document.getElementById('mensagem').innerText = 'Rio de Janeiro';
+});
+
+document.getElementById('state-es').addEventListener('click', function() {
+    document.getElementById('mensagem').innerText = 'Espirito Santo';
+});
+
+document.getElementById('state-mt').addEventListener('click', function() {
+    document.getElementById('mensagem').innerText = 'Mato Grosso';
+});
+
+document.getElementById('state-ms').addEventListener('click', function() {
+    document.getElementById('mensagem').innerText = 'Mato Grosso do Sul';
+});
+
+document.getElementById('state-go').addEventListener('click', function() {
+    document.getElementById('mensagem').innerText = 'Goiás';
+});
+
+document.getElementById('state-pe').addEventListener('click', function() {
+    document.getElementById('mensagem').innerText = 'Pernambuco';
 });
 
 document.getElementById('state-ce').addEventListener('click', function() {
-    document.getElementById('mensagem').innerText = 'Bem-vindo ao Ceará!';
+    document.getElementById('mensagem').innerText = 'Ceará';
 });
 
-document.getElementById('state-rr').addEventListener('click', function() {
-    document.getElementById('mensagem').innerText = 'Bem-vindo a Roraima!';
+document.getElementById('state-se').addEventListener('click', function() {
+    document.getElementById('mensagem').innerText = 'Sergipe';
 });
 
-document.getElementById('state-to').addEventListener('click', function() {
-    document.getElementById('mensagem').innerText = 'Bem-vindo ao Tocantins!';
+document.getElementById('state-ba').addEventListener('click', function() {
+    document.getElementById('mensagem').innerText = 'Bahia';
 });
 
 document.getElementById('state-ma').addEventListener('click', function() {
-    document.getElementById('mensagem').innerText = 'Bem-vindo ao Maranhão!';
+    document.getElementById('mensagem').innerText = 'Manharão';
 });
 
-document.getElementById('state-rn').addEventListener('click', function() {
-    document.getElementById('mensagem').innerText = 'Bem-vindo ao Rio Grande do Norte!';
+document.getElementById('state-pi').addEventListener('click', function() {
+    document.getElementById('mensagem').innerText = 'Piauí';
+});
+
+document.getElementById('state-al').addEventListener('click', function() {
+    document.getElementById('mensagem').innerText = 'Alagoas';
 });
 
 document.getElementById('state-pb').addEventListener('click', function() {
-    document.getElementById('mensagem').innerText = 'Bem-vindo à Paraíba!';
+    document.getElementById('mensagem').innerText = 'Paraíba';
+});
+
+document.getElementById('state-rn').addEventListener('click', function() {
+    document.getElementById('mensagem').innerText = 'Rio Grande do Norte';
+});
+
+document.getElementById('state-am').addEventListener('click', function() {
+    document.getElementById('mensagem').innerText = 'Amazonas';
+});
+
+document.getElementById('state-to').addEventListener('click', function() {
+    document.getElementById('mensagem').innerText = 'Tocantins';
+});
+
+document.getElementById('state-ap').addEventListener('click', function() {
+    document.getElementById('mensagem').innerText = 'Amapá';
+});
+
+document.getElementById('state-ap').addEventListener('click', function() {
+    document.getElementById('mensagem').innerText = 'Acre';
+});
+
+document.getElementById('state-ro').addEventListener('click', function() {
+    document.getElementById('mensagem').innerText = 'Rondônia';
+});
+
+document.getElementById('state-rr').addEventListener('click', function() {
+    document.getElementById('mensagem').innerText = 'Roraima';
+});
+
+document.getElementById('state-pa').addEventListener('click', function() {
+    document.getElementById('mensagem').innerText = 'Pará';
 });
